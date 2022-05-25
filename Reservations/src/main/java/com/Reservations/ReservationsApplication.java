49aebd6fe7c8a32d643af66c7bcb5e228ba2683f
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2022-05-25 17:39:19 +0200

    Izmena boja pri loginu

--- Reservations/src/main/java/com/Reservations/ReservationsApplication.java
+++ Reservations/src/main/java/com/Reservations/ReservationsApplication.java
@@ -3,9 +3,7 @@ package com.Reservations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
-        )
+@SpringBootApplication
 public class ReservationsApplication {
 
 	public static void main(String[] args) {
