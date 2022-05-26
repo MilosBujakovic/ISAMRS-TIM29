56219c85ca7df7f6ab59208749d2b97658e77d20
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2022-05-26 15:46:42 +0200

    #3.3.1/#3.4.1 Oba taska rešena

--- Reservations/src/main/java/com/Reservations/Kontroleri/UslugaKontroler.java
+++ Reservations/src/main/java/com/Reservations/Kontroleri/UslugaKontroler.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.Reservations.Modeli.Korisnik;
 import com.Reservations.Modeli.Usluga;
 import com.Reservations.Servis.KorisnikServis;
 import com.Reservations.Servis.UslugaServis;
@@ -20,13 +20,23 @@ public class UslugaKontroler {
 	@Autowired
 	KorisnikServis korisnikServis;
 	
-	@RequestMapping(value = "/usluga")
-	public String getProfilePage(Model model) {
+	@RequestMapping(value = "klijent/usluga/{id}")
+	public String getProfilePage(Model model, @PathVariable Long id) {
 		System.out.println("Usluga page was called!");
-		Usluga usluga = uslugaServis.findById(0L);
+		Usluga usluga = uslugaServis.findById(id);
 		List<Usluga> lista = uslugaServis.findByInstruktor(5L);
 		model.addAttribute("usluga", usluga);
 		model.addAttribute("uslugeInst", lista);
 		return "uslugaProfil";
+	}
+	
+	@RequestMapping(value = "/usluga/{id}")
+	public String getUnauthServicePage(Model model, @PathVariable Long id) {
+		System.out.println("Usluga page was called!");
+		Usluga usluga = uslugaServis.findById(id);
+		List<Usluga> lista = uslugaServis.findByInstruktor(5L);
+		model.addAttribute("usluga", usluga);
+		model.addAttribute("uslugeInst", lista);
+		return "uslugaOsnovniProfil";
 	}
 }
